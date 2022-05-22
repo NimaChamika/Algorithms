e6df517cb0de5c09de500e080dc12369655d7f68
Nirmal <lynchamika@gmail.com>
2022-05-22 16:03:37 +0530

    +binary search implemented

---
+++ 009 - Algorithms/DivideAndConquer/1_BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009___Algorithms.DivideAndConquer
+{
+    class _1_BinarySearch
+    {
+        internal static void Solution()
+        {
+            int[] arr = new int[] { 1, 4, 6, 8, 10, 15, 25, 28, 35, 42, 44, 46, 50 };
+            Console.WriteLine(BinarySearch(arr,11));
+        }
+
+        private static int BinarySearch(int[] array,int value)
+        {
+            int l = 0;
+            int h = array.Length;
+
+            while(l <= h)
+            {
+                int mid = (l + h) /2;
+
+                if(array[mid] > value)
+                {
+                    h = mid - 1;
+                }
+                else if (array[mid] < value)
+                {
+                    l = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ 009 - Algorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _009___Algorithms.DivideAndConquer;
+
+namespace _009___Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _1_BinarySearch.Solution();
+        }
+    }
+}
